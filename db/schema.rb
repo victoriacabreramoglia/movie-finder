611d31eac57c9e661de7a297334857dcff026c19
victorioc <42121549+victorioc@users.noreply.github.com>
2018-11-15 16:19:50 -0500

    Revert "Demo day build"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_15_181753) do
+ActiveRecord::Schema.define(version: 2018_11_12_170254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,7 +59,6 @@ ActiveRecord::Schema.define(version: 2018_11_15_181753) do
     t.string "access_token_secret"
     t.text "timeline"
     t.json "profile"
-    t.boolean "initialized"
     t.index ["email"], name: "index_users_on_email"
     t.index ["provider"], name: "index_users_on_provider"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
